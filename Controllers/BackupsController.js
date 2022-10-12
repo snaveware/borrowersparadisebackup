6793fbdc89d469ba94494a97796758ebd18436d1
Evans <work.evans020@gmail.com>
2022-10-12 23:38:06 +0300

    confirmed that the backup is not empty, added backups count to 5

--- Controllers/BackupsController.js
+++ Controllers/BackupsController.js
@@ -1,57 +1,77 @@
-const  Logger  = require('../Logger')
-const {Backup} = require('../models')
-const RequestHandler = require('../RequestHandler')
+const Logger = require("../Logger");
+const { Backup } = require("../models");
+const RequestHandler = require("../RequestHandler");
 
 module.exports = class BackupsController {
-
-    static async saveBackup(req,res){
+    static async saveBackup(req, res) {
         try {
-            const quantity = await Backup.countDocuments()
-            if(quantity > 3) {
-                
-                const toDelete= await Backup.find().select('-data -date').limit(1)
-
-                console.log('to delete-------',toDelete._id)
-
-                const deletion = await Backup.deleteOne({_id: toDelete[0]._id})
-
-
-                if(!deletion.acknowledged || deletion.deletedCount != 1){
-                    Logger.warn({message: 'A problem was detected while deleting old backups', deletion: deletion})
-                } 
+            if (!req.body || Object.keys(req.body).length < 1) {
+                RequestHandler.throwError(400, "Empty backup detected")();
             }
 
-            const newBackup = new Backup({data: req.body})
-    
-            const savedBackup = await newBackup.save()
-    
-            RequestHandler.sendSuccess(req.requestId, res, {isSuccessful: true})
+            const quantity = await Backup.countDocuments();
+            if (quantity > 4) {
+                const toDelete = await Backup.find()
+                    .select("-data -date")
+                    .limit(1);
 
+                console.log("to delete-------", toDelete._id);
+
+                const deletion = await Backup.deleteOne({
+                    _id: toDelete[0]._id,
+                });
+
+                if (!deletion.acknowledged || deletion.deletedCount != 1) {
+                    Logger.warn({
+                        message:
+                            "A problem was detected while deleting old backups",
+                        deletion: deletion,
+                    });
+                }
+            }
+
+            const newBackup = new Backup({ data: req.body });
+
+            const savedBackup = await newBackup.save();
+
+            RequestHandler.sendSuccess(req.requestId, res, {
+                isSuccessful: true,
+            });
         } catch (error) {
-            RequestHandler.sendError(req.requestId,res,error)
+            RequestHandler.sendError(req.requestId, res, error);
         }
     }
 
-    static async restoreBackup(req,res){
+    static async restoreBackup(req, res) {
         try {
-            const latestBackup = await Backup.find().limit(1).sort({'_id': -1})
-            if(!latestBackup){
-                Logger.warn('request for latest backup returned a falsey value')
-                RequestHandler.throwError(404,'request for latest backup returned a falsey value')()
-            }else if (latestBackup.length < 1) {
-                Logger.warn('request for latest backup an empty array instead of one backup object')
-                RequestHandler.throwError(404,'request for latest backup returned an empty array instead of one backup object')()
+            const latestBackup = await Backup.find().limit(1).sort({ _id: -1 });
+            if (!latestBackup) {
+                Logger.warn(
+                    "request for latest backup returned a falsey value"
+                );
+                RequestHandler.throwError(
+                    404,
+                    "request for latest backup returned a falsey value"
+                )();
+            } else if (latestBackup.length < 1) {
+                Logger.warn(
+                    "request for latest backup an empty array instead of one backup object"
+                );
+                RequestHandler.throwError(
+                    404,
+                    "request for latest backup returned an empty array instead of one backup object"
+                )();
             }
 
-            RequestHandler.sendSuccess(req.requestId,res,latestBackup[0].data)
+            RequestHandler.sendSuccess(
+                req.requestId,
+                res,
+                latestBackup[0].data
+            );
         } catch (error) {
-            RequestHandler.sendError(req.requestId,res,error)
+            RequestHandler.sendError(req.requestId, res, error);
         }
     }
 
-
-    static async downloadBackup(){
-        
-    }
-    
-}
+    static async downloadBackup() {}
+};
--- index.js
+++ index.js
@@ -1,166 +1,188 @@
-
-const {resolve} = require('path')
+const { resolve } = require("path");
 global.appRoot = resolve(__dirname);
-
 
 /**
  * Third Party Modules
  */
-const express = require('express')
-require('dotenv').config()
-const mongoose = require('mongoose')
-const cors = require('cors')
-const {mkdirSync,existsSync} = require('fs')
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { mkdirSync, existsSync } = require("fs");
 
 /**
  * Custom Modules
  */
 
-const Logger = require('./Logger');
-const RequestHandler = require('./RequestHandler');
-const {DBConnect} = require('./database');
-const {backupsRouter} = require('./routers');
-const {createRequestId,logRequests} = require('./middlewares')
-const sysConfig = require('./Config')
-
-
+const Logger = require("./Logger");
+const RequestHandler = require("./RequestHandler");
+const { DBConnect } = require("./database");
+const { backupsRouter } = require("./routers");
+const { createRequestId, logRequests } = require("./middlewares");
+const sysConfig = require("./Config");
 
 /** log folder */
 
-const folderName = global.appRoot  + '/logs/'
+const folderName = global.appRoot + "/logs/";
 
 try {
-if (!existsSync(folderName)) {
-    mkdirSync(folderName)
-}
+    if (!existsSync(folderName)) {
+        mkdirSync(folderName);
+    }
 } catch (err) {
-console.error(err)
-Logger.error(err)
-process.emit('SIGINT', {reason: "Failed to create logs folder"}) 
+    console.error(err);
+    Logger.error(err);
+    process.emit("SIGINT", { reason: "Failed to create logs folder" });
 }
 
 /** uploads folder */
 
-const dir = global.appRoot + '/uploads';
+const dir = global.appRoot + "/uploads";
 
 try {
-    if (!existsSync(dir)){
-        mkdirSync(dir)
+    if (!existsSync(dir)) {
+        mkdirSync(dir);
     }
-} catch(error) {
-    console.log(error)
-    Logger.error(error)
-    process.emit('SIGINT', {reason: "Failed to create uploads folder"})
+} catch (error) {
+    console.log(error);
+    Logger.error(error);
+    process.emit("SIGINT", { reason: "Failed to create uploads folder" });
 }
-
-
 
 /**
  * High Level Declarations and Functions
-*/
+ */
 
-DBConnect()
+DBConnect();
 
 const app = express();
-const PORT= sysConfig.PORT
-
+const PORT = sysConfig.PORT;
 
 /**
  * Middlewares
  */
 
 app.use(express.json());
-app.use(cors())
-app.use(createRequestId)
-app.use(logRequests)
-
-
+app.use(cors());
+app.use(createRequestId);
+app.use(logRequests);
 
 /**
- * Using root for health check
+ * Using root and /health for health check
  */
-app.get('/health', (req,res)=>{
-RequestHandler.sendSuccess(req.requestId,res,"Borrower's Paradise Backup Server is Up and Running")
-})
 
-const {sign} = require('jsonwebtoken')
+app.get("/", (req, res) => {
+    RequestHandler.sendSuccess(
+        req.requestId,
+        res,
+        "Borrower's Paradise Backup Server is Up and Running"
+    );
+});
 
-const token  = sign({
-    description: "A KEY TO AUTHENTICATE WITH BORROWER'S PARADISE BACKUP SERVER",
-    exp: Math.floor(Date.now() / 1000) + (86400 * 365)
-},sysConfig.SECRET)
+app.get("/health", (req, res) => {
+    RequestHandler.sendSuccess(
+        req.requestId,
+        res,
+        "Borrower's Paradise Backup Server is Up and Running"
+    );
+});
 
-console.log("A Token: ",token)
+const { sign } = require("jsonwebtoken");
+
+const token = sign(
+    {
+        description:
+            "A KEY TO AUTHENTICATE WITH BORROWER'S PARADISE BACKUP SERVER",
+        exp: Math.floor(Date.now() / 1000) + 86400 * 365,
+    },
+    sysConfig.SECRET
+);
+
+console.log("A Token: ", token);
 
 /**
  * Routers
  */
-app.use('/backup', backupsRouter);
-
+app.use("/backup", backupsRouter);
 
 /**
  * Other routes are recorded as 404 and 500
  */
-app.get('*',( req, res) => {
-    
-    RequestHandler.sendErrorMessage(req.requestId,res,404,'The GET route you are trying to reach is not available' );
-    
-})
+app.get("*", (req, res) => {
+    RequestHandler.sendErrorMessage(
+        req.requestId,
+        res,
+        404,
+        "The GET route you are trying to reach is not available"
+    );
+});
 
-app.post('*',( req, res) => {
-    
-    RequestHandler.sendErrorMessage(req.requestId,res,404,'The POST route you are trying to reach is not available' );
-    
-})
+app.post("*", (req, res) => {
+    RequestHandler.sendErrorMessage(
+        req.requestId,
+        res,
+        404,
+        "The POST route you are trying to reach is not available"
+    );
+});
 
-app.put('*',( req, res) => {
-    
-    RequestHandler.sendErrorMessage(req.requestId,res,404,'The PUT route you are trying to reach is not available' );
-    
-})
-app.patch('*',( req, res) => {
-    RequestHandler.sendErrorMessage(req.requestId,res,404,'The PATCH route you are trying to reach is not available' );
-    
-})
+app.put("*", (req, res) => {
+    RequestHandler.sendErrorMessage(
+        req.requestId,
+        res,
+        404,
+        "The PUT route you are trying to reach is not available"
+    );
+});
+app.patch("*", (req, res) => {
+    RequestHandler.sendErrorMessage(
+        req.requestId,
+        res,
+        404,
+        "The PATCH route you are trying to reach is not available"
+    );
+});
 
-app.delete('*',( req, res) => {
-    RequestHandler.sendErrorMessage(req.requestId,res,404,'The DELETE route you are trying to reach is not available' );
-    
-})
+app.delete("*", (req, res) => {
+    RequestHandler.sendErrorMessage(
+        req.requestId,
+        res,
+        404,
+        "The DELETE route you are trying to reach is not available"
+    );
+});
 
 /**
  * Connecting to database before starting the server
-*/
+ */
 
-mongoose.connection.on('connected', async () =>{
+mongoose.connection.on("connected", async () => {
+    console.log("Database Connected successfully");
 
-console.log('Database Connected successfully')
+    Logger.info("Starting the server...");
 
-    
+    app.listen(PORT || 5000, () => {
+        if (!PORT) {
+            console.log("Server Running on the Default Port 5000");
+            return;
+        }
 
-Logger.info('Starting the server...')
+        console.log(`Server Started on Runtime Port ${PORT} ...`);
 
-app.listen(PORT || 5000 ,() =>{
-    if(!PORT){
-        console.log('Server Running on the Default Port 5000')
-        return;
-    }
-        
-    console.log(`Server Started on Runtime Port ${PORT} ...`)
+        console.log("---all good---");
+    });
+});
 
-    console.log('---all good---')
-
-})  
-    
-})
-
-
-process.on('SIGINT', (info) => {
-Logger.warn(`Stopping Server   ${info.reason?  'Reason: '+ info.reason : 'Unknown reason' }`)
-console.error(`Stopping Server... ${info.reason ? 'Reason: '+ info.reason : 'Unknown reason'}`)
-process.exit()
-})
- 
- 
- 
- 
+process.on("SIGINT", (info) => {
+    Logger.warn(
+        `Stopping Server   ${
+            info.reason ? "Reason: " + info.reason : "Unknown reason"
+        }`
+    );
+    console.error(
+        `Stopping Server... ${
+            info.reason ? "Reason: " + info.reason : "Unknown reason"
+        }`
+    );
+    process.exit();
+});
